a4a1e6305c541cde059c92938efa1b3f5660ac78
slacker3007 <dwzeref@gmail.com>
2022-01-11 16:07:59 +0200

    v1

---
+++ CPU_Temperature.py
@@ -0,0 +1,43 @@
+from gpiozero import CPUTemperature
+import statistics
+import time
+
+cpu = CPUTemperature()
+cpu = str(cpu).replace("<gpiozero.CPUTemperature object temperature=", "")
+cpu = str(cpu).replace(">", "")
+# print (cpu)
+
+temp1 = float(cpu)
+# print (temp1)
+
+time.sleep(1)
+cpu = CPUTemperature()
+cpu = str(cpu).replace("<gpiozero.CPUTemperature object temperature=", "")
+cpu = str(cpu).replace(">", "")
+temp2 = float(cpu)
+# print (temp2)
+
+time.sleep(1)
+cpu = CPUTemperature()
+cpu = str(cpu).replace("<gpiozero.CPUTemperature object temperature=", "")
+cpu = str(cpu).replace(">", "")
+temp3 = float(cpu)
+# print (temp3)
+
+time.sleep(1)
+cpu = CPUTemperature()
+cpu = str(cpu).replace("<gpiozero.CPUTemperature object temperature=", "")
+cpu = str(cpu).replace(">", "")
+temp4 = float(cpu)
+# print (temp4)
+
+time.sleep(1)
+cpu = CPUTemperature()
+cpu = str(cpu).replace("<gpiozero.CPUTemperature object temperature=", "")
+cpu = str(cpu).replace(">", "")
+temp5 = float(cpu)
+# print (temp5)
+
+orders = [temp1, temp2, temp3, temp4, temp5]
+average = statistics.mean(orders)
+print('CPU_Temperature', str(round(average, 2)))
